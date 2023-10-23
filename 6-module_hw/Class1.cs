2e68cce7a66807a726eacbfc7624de04638f9b57
AidynbekZh <aidinbek467@gmail.com>
2023-10-23 17:11:19 +0600

    Добавьте файлы проекта.

---
+++ 6-module_hw/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_module_hw
+{
+    public class Class1
+    {
+    }
+}
